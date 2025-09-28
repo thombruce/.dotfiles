c216f81c66dd327a6832be6faef76d67ec0197cb
Thom Bruce <thom@thombruce.com>
2025-09-28 16:00:31 +0100

    add nvim-surround and markdown-preview

--- .config/nvim/lua/plugins/lang--autopairs.lua
+++ .config/nvim/lua/plugins/lang--autopairs.lua
@@ -1,11 +1,5 @@
 return {
   -- Autopairs plugin for automatic completion of pairs like {}, "", (), etc.
-  -- Other options exist and we should maybe assess...
-  -- - mini.surround
-  -- - tpope/vim-surround
-  -- - machakann/vim-sandwich
-  -- - kylechui/nvim-surround (which is maybe just tpope/vim-surround rewritten in Lua?)
-  -- - mini.ai (which is listed for comparison by mini.surround and might also be worth assessing)
   {
     'windwp/nvim-autopairs',
     enabled = true,
--- .config/nvim/lua/plugins/ui--noice.lua
+++ .config/nvim/lua/plugins/ui--noice.lua
@@ -15,5 +15,16 @@ return {
       --   If not available, we use `mini` as the fallback
       "rcarriga/nvim-notify",
     },
+    config = function()
+      require("noice").setup({
+        lsp = {
+          override = {
+            ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+            ["vim.lsp.util.stylize_markdown"] = true,
+            -- ["cmp.entry.get_documentation"] = true, -- requires hrsh7th/nvim-cmp
+          },
+        },
+      })
+    end,
   },
 }

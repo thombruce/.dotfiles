d0f1d343afe3c19ea431c877a3f3695a3f92dc18
Thom Bruce <thom@thombruce.com>
2025-10-08 01:06:13 +0100

    Setup Yazi file explorer, zoxide cd replacement with memory and eza prettier ls alternative

--- .thombruce.zsh
+++ .thombruce.zsh
@@ -1,3 +1,17 @@
 # Custom Config for Zsh
 
 alias dotfiles='git --git-dir=$HOME/.dotfiles --work-tree=$HOME'
+
+alias ls='eza --icons=auto --level=2'
+
+function y() {
+	local tmp="$(mktemp -t "yazi-cwd.XXXXXX")" cwd
+	yazi "$@" --cwd-file="$tmp"
+	IFS= read -r -d '' cwd < "$tmp"
+	[ -n "$cwd" ] && [ "$cwd" != "$PWD" ] && builtin cd -- "$cwd"
+	rm -f -- "$tmp"
+}
+
+# Explicitly should come near END of file (after compinit)
+eval "$(zoxide init zsh)"
+alias cd='z'

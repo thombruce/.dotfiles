a5310e68d41ca0f17ade6b91ea1bcf663099a1fb
Thom Bruce <thom@thombruce.com>
2025-10-16 21:35:41 +0100

    Set neovim border style for floating windows

--- .config/nvim/lua/config/ui.lua
+++ .config/nvim/lua/config/ui.lua
@@ -5,3 +5,6 @@ vim.wo.relativenumber = true
 -- Hides the command line when not in active use
 -- (This might be redundant since installing Noice? ... But we might get rid of Noice)
 vim.o.cmdheight = 0
+
+-- Set default border for floating windows
+vim.opt.winborder = "rounded"
--- .config/nvim/lua/plugins/ui--oil.lua
+++ .config/nvim/lua/plugins/ui--oil.lua
@@ -20,7 +20,6 @@ return {
       },
       float = {
         padding = 8,
-        border = "rounded",
       },
     },
     -- Optional dependencies

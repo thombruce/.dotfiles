f0597397802a8bd49b186511643f6b561eb3a28d
Thom Bruce <thom@thombruce.com>
2025-11-03 03:05:48 +0000

    Amend locale (Ansible requires that we explicitly set a UTF-8 locale)

--- .thombruce.zsh
+++ .thombruce.zsh
@@ -6,7 +6,7 @@ fi
 
 # Results in a more expected sort order for non-alphanumeric
 # characters when using sort
-export LC_ALL=C
+export LC_ALL=C.UTF-8
 
 # Allow user-wide installation of NPM packages
 PATH="$HOME/.local/bin:$PATH"

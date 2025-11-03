6783424b110f1f0212cf319c9bb1ac5d80f7570b
Thom Bruce <thom@thombruce.com>
2025-11-03 18:52:04 +0000

    Enable tab completion in Neovim (muscle memory)

--- .config/nvim/lua/plugins/lang--blink.lua
+++ .config/nvim/lua/plugins/lang--blink.lua
@@ -31,7 +31,7 @@ return {
       -- C-k: Toggle signature help (if signature.enabled = true)
       --
       -- See :h blink-cmp-config-keymap for defining your own keymap
-      keymap = { preset = 'default' },
+      keymap = { preset = 'super-tab' },
 
       appearance = {
         -- 'mono' (default) for 'Nerd Font Mono' or 'normal' for 'Nerd Font'
@@ -40,7 +40,7 @@ return {
       },
 
       -- (Default) Only show the documentation popup when manually triggered
-      completion = { documentation = { auto_show = false } },
+      completion = { documentation = { auto_show = true } },
 
       -- Default list of enabled providers defined so that you can extend it
       -- elsewhere in your config, without redefining it, due to `opts_extend`

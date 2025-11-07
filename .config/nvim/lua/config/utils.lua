1ae1c30c7e9845a50e02d19ee12ad5fdf9813642
Thom Bruce <thom@thombruce.com>
2025-11-07 18:30:55 +0000

    Add timed highlight to yank in Neovim (thank you, @smnatale!)

--- .config/nvim/lua/config/utils.lua
+++ .config/nvim/lua/config/utils.lua
@@ -13,3 +13,13 @@ vim.filetype.add({
     [".*%.todo%.txt"] = "todotxt",
   },
 })
+
+-- highlight yank
+vim.api.nvim_create_autocmd("TextYankPost", {
+  group = vim.api.nvim_create_augroup("highlight_yank", { clear = true }),
+  pattern = "*",
+  desc = "highlight selection on yank",
+  callback = function()
+    vim.highlight.on_yank({ timeout = 200, visual = true })
+  end,
+})

3109dd943c8bb67a3a4b441dc990fd734370e70b
Thom Bruce <thom@thombruce.com>
2026-01-31 18:29:13 +0000

    Update Neovim packages

---
+++ .config/nvim/lua/plugins/ui--bufferline.lua
@@ -0,0 +1,14 @@
+return {
+  {
+    'akinsho/bufferline.nvim',
+    version = "*",
+    dependencies = 'nvim-tree/nvim-web-devicons',
+    config = function()
+      require("bufferline").setup {
+        options = {
+          -- separator_style = 'slope',
+        }
+      }
+    end,
+  }
+}
--- .config/nvim/lua/plugins/ui--lualine.lua
+++ .config/nvim/lua/plugins/ui--lualine.lua
@@ -16,29 +16,29 @@ return {
         sections = {
           lualine_x = { 'encoding', 'fileformat', { 'filetype' }, { 'lsp_status' } },
         },
-        tabline = {
-          lualine_a = {
-            {
-              'buffers',
-              filetype_names = {
-                oil = 'Oil',
-                toggleterm = 'Terminal',
-              },
-              symbols = {
-                alternate_file = false,
-              },
-            }
-          },
-          lualine_b = {},
-          lualine_c = {},
-          lualine_x = {},
-          lualine_y = {
-            { 'datetime', style = '%Y-%m-%d %a' },
-          },
-          lualine_z = {
-            { 'datetime', style = '%H:%M' },
-          }
-        },
+        -- tabline = {
+        --   lualine_a = {},
+        --   lualine_b = {
+        --     {
+        --       'buffers',
+        --       filetype_names = {
+        --         oil = 'Oil',
+        --         toggleterm = 'Terminal',
+        --       },
+        --       symbols = {
+        --         alternate_file = false,
+        --       },
+        --     }
+        --   },
+        --   lualine_c = {},
+        --   lualine_x = {},
+        --   lualine_y = {
+        --     { 'datetime', style = '%Y-%m-%d %a' },
+        --   },
+        --   lualine_z = {
+        --     { 'datetime', style = '%H:%M' },
+        --   }
+        -- },
         extensions = {}
       })
     end,

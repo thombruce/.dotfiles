292f7c58b5fcf62c6ce5fbd6c0611f821de70fed
Thom Bruce <thom@thombruce.com>
2026-04-01 03:05:00 +0100

    Add vim as Lua global

--- .config/nvim/lua/lsp/init.lua
+++ .config/nvim/lua/lsp/init.lua
@@ -12,3 +12,13 @@ vim.lsp.enable({
   'ts_ls',
   'vue_ls',
 })
+
+vim.lsp.config('lua_ls', {
+  settings = {
+    Lua = {
+      diagnostics = {
+        globals = {'vim'},
+      },
+    },
+  },
+})
